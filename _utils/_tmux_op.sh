7d325b27e759cfb8425cf82c88fdb1807d84fb17
---- <lukasz.czernik@gmail.com>
2024-09-30 21:36:41 +0200

    refactor: Improve function documentation and return statements in tmux operations script

--- _utils/_tmux_op.sh
+++ _utils/_tmux_op.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
+
 function ssh_password(){
+    # Adds sshpass string to ssh command
+    # input: none
+    # output: <str:sshpass_cmd>
+    # return: <int:0||1>
+
     if [[ -z ${DEFAULT_PASSWORD} ]]
     then
         echo ""
-        exit 0
+        return 0
     fi
     echo "sshpass -p '$DEFAULT_PASSWORD' "
-    exit 0
+    return 0
 }
 
 function start_tmux_ssh_session(){
-    # Stargs session tmux session
-    # usage: start_tmux_ssh_session <ip> <port> <group_name> <name> <user>
-    # input: <ip> <port> <group_name> <name> <user>
-    # output: exit 0 or 1
+    # Starts session tmux session
+    # input: <str:ip> <str:port> <str:group_name> <str:name> <str:user>
+    # output: none
+    # return: <int:0||1>
     local ip=$1
     local port=$2
     local group_name=$3
@@ -25,44 +31,51 @@ function start_tmux_ssh_session(){
     ${DEFAULT_EXTRA} \
      ${user}@${ip} \
     -p ${port}") &>/dev/null
+
+    # Check the output code
+    sleep 0.1
+    check_tmux_ssh_session $group_name $name &>/dev/null # Returns 0 or 1
+    return $?
 }
 
 function kill_tmux_ssh_sessioin(){
     # Kills existing tmux session
-    # usage: kill_tmux_ssh_session <group_name> <name>
-    # input: <group_name> <name>
-    # output: exit 0 or 1
+    # input: <str:group_name> <str:name>
+    # output: none
+    # return: <int:0||1>
     local group_name=$1
     local name=$2
     (tmux kill-session -t "${group_name}-${name}") &>/dev/null
+    return $?
 }
 
 function check_tmux_ssh_session(){
     # Verifys if tmux session is opened with specified host
-    # usage: check_tmux_ssh_session <group_name> <name>
-    # input: <group_name> <name>
-    # output: <"linked" || "unlinked">
+    # input: <str:group_name> <str:name>
+    # output: <str:"linked"||"unlinked">
+    # return: <int:0||1>
     if tmux ls | grep -q "${group_name}-${name}"
     then
         echo "linked"
+        return 0
     else
         echo "unlinked"
+        return 1
     fi
 }
 
 function execute_tmux_ssh_command(){
     # Executes give command in existing tmux session
-    # usage: kill_tmux_ssh_session <group_name> <name> <command>
-    # input: <group_name> <name> <command>
-    # output: <tmux command output>
+    # input: <str:group_name> <str:name> <str:command>
+    # output: <str:tmux_command_output>
+    # return: <int:0||1>
     local group_name=$1
     local name=$2
     local command=$3
     local signal_done='678e5e019a79526d0fcca5e29f6e5f78'
 
     local before=$(tmux capture-pane -p -S '-' -J -t "${group_name}-${name}")
-    tmux send-keys -t "${group_name}-${name}" "$command ;touch /tmp/$signal_done" C-m
-
+    tmux send-keys -t "${group_name}-${name}" "$command ;echo $? > /tmp/$signal_done" C-m
 
     (tmux new-session -d -s "${group_name}-${name}" \
     "$(ssh_password)ssh \
@@ -70,7 +83,7 @@ function execute_tmux_ssh_command(){
     ${DEFAULT_EXTRA} \
      ${user}@${ip} \
     -p ${port}") &>/dev/null
-    
+
     # Wait for signal done
     while true; do
         sleep 0.5s
