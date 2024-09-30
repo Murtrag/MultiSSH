923ca76819fca413ed43d28e8865af96b624793b
---- <lukasz.czernik@gmail.com>
2024-09-30 20:06:45 +0200

    Add global configuration

---
+++ global.sh
@@ -0,0 +1,15 @@
+# SSH Auth config
+
+DEFAULT_USER="vagrant"
+
+# To use default password, the package sshpass is required
+# DEFAULT_PASSWORD="passwrod123"
+
+DEFAULT_PORT="22"
+
+# DEFAULT_IDENTITY="-i /home/vagrant/.ssh/my_vagrant_key"
+
+DEFAULT_EXTRA="-o StrictHostKeyChecking=no"
+
+# PREFIX= "sshpass -p 'YourGlobalPassword'"
+~                                                  
